81bf5c64987a773996bf646e813f2e7dd67cd505
eric <eric@lytics.io>
2015-11-07 00:30:43 -0800

    removing go 1.3 tests

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -2,7 +2,7 @@
 set -ev
 
 
-go test -race -cpu 2,4 -v -timeout 5m ./... 2>&1 | grep -v 'etcdserver:' | grep -v 'raft:'
+go test -race -cpu 2,4 -v -timeout 5m 2>&1 | grep -v 'etcdserver:' | grep -v 'raft:'
 
 
 
